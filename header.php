a938490043ee8f7a75ce128eaa024a65b8552b38
thisismesaurav <thisisme.saurav@gmail.com>
2026-07-03 22:38:18 +0530

    Moved Category and Type of Product submenus under Products and removed Categories menu

--- header.php
+++ header.php
@@ -109,29 +109,16 @@ if ( empty( $brand_logo ) ) {
                                       <i class="las la-minus"></i><span>Product</span>
                                   </a>
                               </li>
-                          </ul>
-                      </li>
-                      <li class=" ">
-                          <a href="#category" class="collapsed" data-toggle="collapse" aria-expanded="false">
-                              <svg class="svg-icon" id="p-dash3" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-                                  <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-                              </svg>
-                              <span class="ml-4">Categories</span>
-                              <svg class="svg-icon iq-arrow-right arrow-active" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                  <polyline points="10 15 15 20 20 15"></polyline><path d="M4 4h7a4 4 0 0 1 4 4v12"></path>
-                              </svg>
-                          </a>
-                          <ul id="category" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                                  <li class="">
-                                          <a href="<?php echo esc_url( $home_url . 'list-category' ); ?>">
-                                              <i class="las la-minus"></i><span>Category</span>
-                                          </a>
-                                  </li>
-                                  <li class="">
-                                          <a href="<?php echo esc_url( $home_url . 'list-type' ); ?>">
-                                              <i class="las la-minus"></i><span>Type of Product</span>
-                                          </a>
-                                  </li>
+                              <li class="">
+                                  <a href="<?php echo esc_url( $home_url . 'list-category' ); ?>">
+                                      <i class="las la-minus"></i><span>Category</span>
+                                  </a>
+                              </li>
+                              <li class="">
+                                  <a href="<?php echo esc_url( $home_url . 'list-type' ); ?>">
+                                      <i class="las la-minus"></i><span>Type of Product</span>
+                                  </a>
+                              </li>
                           </ul>
                       </li>
                       <li class=" ">
