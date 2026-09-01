6fded22c3b5b9f6b930b7a44341021d9f148e495
thisismesaurav <thisisme.saurav@gmail.com>
2026-09-01 19:59:16 +0530

    Show Category ID in Category List view table

--- index.php
+++ index.php
@@ -689,6 +689,7 @@ if ( strpos( $view, 'list-category' ) !== false ) {
                 $img_url = get_template_directory_uri() . '/' . $img_url;
             }
             $tbody .= '<tr>';
+            $tbody .= '<td class="text-muted" style="font-size:12px;">#' . esc_html( $cat->id ) . '</td>';
             $tbody .= '<td><div class="d-flex align-items-center"><img src="' . esc_url( $img_url ) . '" class="img-fluid rounded avatar-50 mr-3" alt="image"><div>' . esc_html( $cat->name ) . '</div></div></td>';
             $tbody .= '<td>' . esc_html( $cat->name ) . '</td>';
             $tbody .= '<td>';
@@ -709,7 +710,7 @@ if ( strpos( $view, 'list-category' ) !== false ) {
             $tbody .= '</tr>';
         }
     } else {
-        $tbody .= '<tr><td colspan="3" class="text-center">No categories found.</td></tr>';
+        $tbody .= '<tr><td colspan="4" class="text-center">No categories found.</td></tr>';
     }
     $tbody .= '</tbody>';
     $content = preg_replace( '/<tbody class="ligth-body">.*?<\/tbody>/s', $tbody, $content );
